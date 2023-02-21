c2445940646c746091a716956edc2d33c26c5744
Francesco Paolo Piga <fppiga@outlook.com>
2023-02-21 03:23:33 +0100

    Created Employee and Department classes

---
+++ WiredBrainCoffee.EmployeeManager/Data/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WiredBrainCoffee.EmployeeManager.Data.Models
+{
+	public class Department
+	{
+		public int Id { get; set; }
+		[Required]
+		[StringLength(50)]
+		public string? Name { get; set; }
+		public List<Employee> Employees { get; set; } = new();
+	}
+}
+
