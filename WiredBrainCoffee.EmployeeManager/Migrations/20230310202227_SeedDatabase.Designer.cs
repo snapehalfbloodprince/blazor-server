c57f0fc4e75d1853fdddca184c125daa01f08eb0
Francesco Paolo Piga <fppiga@outlook.com>
2023-03-10 21:25:00 +0100

    Aggiunti dati di seed al db e creata migrazione EF per Update-Database

--- WiredBrainCoffee.EmployeeManager/Migrations/20230310202227_SeedDatabase.Designer.cs
+++ WiredBrainCoffee.EmployeeManager/Migrations/20230310202227_SeedDatabase.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WiredBrainCoffee.EmployeeManager.Data;
 
@@ -11,9 +12,11 @@ using WiredBrainCoffee.EmployeeManager.Data;
 namespace WiredBrainCoffee.EmployeeManager.Migrations
 {
     [DbContext(typeof(EmployeeManagerDbContext))]
-    partial class EmployeeManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230310202227_SeedDatabase")]
+    partial class SeedDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
